362c9a73b1bc73f31a78bb3123aa87a8548a8bf5
Daniel <dev@salvagelabs.com.au>
2017-02-08 15:07:55 +0000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,6 @@
+/* Webpack entry file */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
