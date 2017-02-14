b8887d2fc900e7543579858c78b6d5898cc24e4a
Daniel <dev@salvagelabs.com.au>
2017-02-14 10:01:39 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      {test: /\.scss$/, exclude: /node_modules/, loaders: ['style-loader', 'css-loader', 'sass-loader']}
+      { test: /\.scss$/, exclude: /node_modules/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
